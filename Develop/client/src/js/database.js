9b380554d14c1a40a5af1563cba217edf9d4e34e
saraschroeder <sara.schroeder00@gmail.com>
2023-04-25 15:17:16 -0500

    completed TODOs in src folder

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -11,11 +11,37 @@ const initdb = async () =>
       console.log('jate database created');
     },
   });
+export const putDb = async (content) => {
+  console.log("PUT to the database");
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+  const jateDb = await openDB("jate", 1);
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+  const tx = jateDb.transaction("jate", "readwrite");
+
+  const store = tx.objectStore("jate");
+
+  const request = store.put({id: 1, value: content});
+
+  const result = await request;
+    console.log("🚀 - data saved to the database", result);
+};
+
+export const getDb = async () => {
+  console.log("GET from the database");
+
+  const jateDb = await openDB("jate", 1);
+
+  const tx = jateDb.transaction("jate", "readonly");
+
+  const store = tx.objectStore("jate");
+
+  const request = store.get(1);
+
+  const result = await request;
+
+    console.log("🚀 - data retrieved from the database", result.value)
+	  console.log("🚀 - data not found in the database");
+  return result?.value;
+};
 
 initdb();
